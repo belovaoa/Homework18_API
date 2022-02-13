b28c244e66cdaebfb53ee614c7b2ba69e5e461d1
belova_oa <belovva.o@gmail.com>
2022-02-13 15:02:41 +0300

    some changes

--- src/test/java/com/belovaoa/config/UserCredential.java
+++ src/test/java/com/belovaoa/config/UserCredential.java
@@ -5,7 +5,10 @@ import org.aeonbits.owner.Config;
 @Config.Sources({"classpath:config/credential.properties"})
 public interface UserCredential extends Config {
 
+    @DefaultValue("testfordemowebshop@mail.ru")
     String email();
+
+    @DefaultValue("6210test")
     String password();
     //String cookie();
 }
